c82c31e609086a9fa373cebd12e0402eaac1dfa6
GCRY6 <2381165671@qq.com>
2025-05-18 12:14:07 +0800

    练习

--- main.java
+++ main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("太不容易了");
+        char a = 1;
+        a=10000;
+        System.out.println("Hello world!");
+        System.out.println(0);
+        System.out.println("1.666"+a);
+        System.out.println(true);
+        System.out.println("H");
     }
 }
