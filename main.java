a349a844a3171e7e5234101a0b29634401fb3e92
GCRY6 <2381165671@qq.com>
2025-05-18 13:19:23 +0800

    更改类型

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        char a = 1;
+        float a = 1;
         a=10000;
         System.out.println("Hello world!");
         System.out.println(0);
